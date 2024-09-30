837ad3ed8363c7ee1884c85b6cc24475b7a66691
NI9LOGIC <rehmanhassan117@gmail.com>
2024-09-30 10:33:15 +0500

    Post Upload Successfull

--- app/Login/components/LoginForm.tsx
+++ app/Login/components/LoginForm.tsx
@@ -1,19 +1,55 @@
 "use client";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import { auth, firestore } from "@/firebase/firebase";
 import { doc, getDoc, setDoc } from "firebase/firestore";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 import { Icons } from "@/app/Components/ui/Icons";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import type { User } from "firebase/auth";
+
 const LoginForm = () => {
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        setUser(user);
+        const userDoc = await getDoc(doc(firestore, "users", user.uid));
+        if (userDoc.exists()) {
+        }
+      } else {
+        router.push("/Login");
+      }
+
+      //   Cleanup function
+    });
+    return () => unsubscribe();
+  }, [router]);
+
+  const handleLogout = async () => {
+    try {
+      setLoading(true);
+      await signOut(auth);
+      router.push("/");
+      toast.error("Logged out successfully");
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      toast.error("An error occurred");
+      setLoading(false);
+    }
+  };
   const handleLogin = async (event: React.FormEvent) => {
     event.preventDefault();
     setLoading(true);
@@ -61,38 +97,50 @@ const LoginForm = () => {
     }
   };
   return (
-    <form onSubmit={handleLogin}>
-      <Label>Email</Label>
-      <Input
-        type="email"
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-        required
-      />
-      <Label>Password</Label>
-      <Input
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-        type="password"
-        required
-      />
+    <div>
+      {!user ? (
+        <form onSubmit={handleLogin}>
+          <Label>Email</Label>
+          <Input
+            type="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required
+          />
+          <Label>Password</Label>
+          <Input
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            required
+          />
 
-      <p className="text-[12px] flex flex-row gap-2">
-        Don&apos;t have an account?{" "}
-        <a href="/Register" className="text-blue-500 underline">
-          Sign Up
-        </a>{" "}
-      </p>
-      <Button type="submit" className="mt-5">
-        {loading ? (
-          <Icons.spinner className="w-8 h-8 animate animate-spin" />
-        ) : (
-          "Log In"
-        )}
-      </Button>
-    </form>
+          <p className="text-[12px] flex flex-row gap-2">
+            Don&apos;t have an account?{" "}
+            <a href="/Register" className="text-blue-500 underline">
+              Sign Up
+            </a>{" "}
+          </p>
+          <Button type="submit" className="mt-5">
+            {loading ? (
+              <Icons.spinner className="w-8 h-8 animate animate-spin" />
+            ) : (
+              "Log In"
+            )}
+          </Button>
+        </form>
+      ) : (
+        <Button onClick={async () => handleLogout()}>
+          {loading ? (
+            <Icons.spinner className="animate animate-spin h-8 w-8" />
+          ) : (
+            <p>Log Out</p>
+          )}
+        </Button>
+      )}
+    </div>
   );
 };
 
--- app/Publish/components/Publish.tsx
+++ app/Publish/components/Publish.tsx
@@ -1,59 +1,183 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { onAuthStateChanged, signOut } from "firebase/auth";
-import { auth, firestore } from "@/firebase/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth, firestore, storage } from "@/firebase/firebase"; // Make sure to import storage and db
+import React, { useEffect, useState } from "react";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { addDoc, collection } from "firebase/firestore";
 import type { User } from "firebase/auth";
-import { doc, getDoc } from "firebase/firestore";
-import { useEffect, useState } from "react";
-import toast from "react-hot-toast";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import toast from "react-hot-toast";
 import { Icons } from "@/app/Components/ui/Icons";
+
 const Publish = () => {
-  const [user, setUser] = useState<User | null>(null);
-  const [username, setUsername] = useState<string | null>(null);
-  const [loading, setLoading] = useState(false);
   const router = useRouter();
+  const [user, setUser] = useState<User | null>(null);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [content, setContent] = useState("");
+  const [imageFile, setImageFile] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        setUser(user);
-        const userDoc = await getDoc(doc(firestore, "users", user.uid));
-        if (userDoc.exists()) {
-          const userData = userDoc.data();
-          setUsername(userData.name);   
-        }
+    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
       } else {
         router.push("/Login");
       }
-
-      //   Cleanup function
     });
+
+    // Cleanup function
     return () => unsubscribe();
   }, [router]);
 
-  const handleLogout = async () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleImageChange = (e: any) => {
+    if (e.target.files[0]) {
+      setImageFile(e.target.files[0]);
+    }
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setLoading(true);
     try {
-      setLoading(true);
-      await signOut(auth);
-      router.push("/");
-      toast.error("Logged out successfully");
+      // Check if there is an image to upload
+      let imageUrl = "";
+      if (imageFile) {
+        // @ts-expect-error Shall work as expected
+        const imageRef = ref(storage, `posts/${imageFile.name}-${Date.now()}`);
+        await uploadBytes(imageRef, imageFile);
+        imageUrl = await getDownloadURL(imageRef);
+      }
+
+      // Add form data and image URL to Firestore
+      await addDoc(collection(firestore, "posts"), {
+        author: user?.email,
+        title,
+        description,
+        category,
+        content,
+        imageUrl, // Store the image URL in Firestore
+        createdAt: new Date(),
+        userId: user?.uid, // Reference to the user who created the post
+      });
+
+      // Reset form and redirect
+      setTitle("");
+      setDescription("");
+      setCategory("");
+      setContent("");
+      setImageFile(null);
+      router.push("/"); // Redirect to home or posts list page
       setLoading(false);
+      toast.success("Post published successfully");
     } catch (error) {
-      console.error(error);
+      console.error("Error publishing post: ", error);
       toast.error("An error occurred");
       setLoading(false);
     }
   };
+
   return (
-    <div>
-      <Button onClick={async () => handleLogout()}>
-        {loading ? (
-          <Icons.spinner className="animate animate-spin h-8 w-8" />
-        ) : (
-          <p>Log Out {user?.email} - {username}</p>
-        )}
-      </Button>
+    <div className="p-24">
+      <Card>
+        <CardHeader>
+          <CardTitle>Publish A Post</CardTitle>
+          <CardDescription>
+            Publish your own post by filling out the form below
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit}>
+            <Label>Title</Label>
+            <Input
+              placeholder="Put a title here"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              required
+              disabled={loading}
+            />
+            <Label>Description</Label>
+            <Input
+              placeholder="Put a description here"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              required
+              disabled={loading}
+            />
+            <Label>Category</Label>
+            <Select
+              onValueChange={(e) => {
+                setCategory(e);
+              }}
+              disabled={loading}
+            >
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Category" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="Fashion">Fashion</SelectItem>
+                <SelectItem value="Food">Food</SelectItem>
+                <SelectItem value="Coding">Coding</SelectItem>
+                <SelectItem value="Style">Style</SelectItem>
+                <SelectItem value="Travel">Travel</SelectItem>
+                <SelectItem value="Culture">Culture</SelectItem>
+              </SelectContent>
+            </Select>
+            <Label>Content</Label>
+            <textarea
+              id="message"
+              rows={4}
+              className="block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300"
+              placeholder="Write your thoughts here..."
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              required
+              disabled={loading}
+            />
+
+            <Label>Image</Label>
+            <Input
+              disabled={loading}
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+            />
+
+            <CardFooter className="mt-4">
+              <Button type="submit" disabled={loading}>
+                {loading ? (
+                  <Icons.spinner className="animate animate-spin w-8 h-8" />
+                ) : (
+                  "Publish"
+                )}
+              </Button>
+            </CardFooter>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 };

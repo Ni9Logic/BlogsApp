af02ef5756880f5fe1d00810e856aa418689e620
NI9LOGIC <rehmanhassan117@gmail.com>
2024-09-30 12:29:45 +0500

    ..

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -9,13 +9,7 @@ import {
 } from "@/components/ui/card";
 import LoginForm from "./components/LoginForm";
 
-// TODO: 1. Create sign up page, then register the user in the database via the youtube tutorial.
-// TODO: 2. Login the user, learn about auth session with firebase.
-// TODO: 3. Only logged in users can publish posts.
-// TODO: 4. Learn about uploading images with firebase.
-// TODO: 5. Learn about fetching data from firebase.
-// TODO: 6. Make sure page is responsive.
-// TODO: 7. Page revalidation as well.
+
 export default function page() {
   return (
     <div className="p-24">
